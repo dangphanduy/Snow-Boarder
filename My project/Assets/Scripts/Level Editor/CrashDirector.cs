e249a523511f204b35d38903f2d5753c73feb30d
dangphanduy <117800872+dangphanduy@users.noreply.github.com>
2025-03-21 11:59:54 +0700

    Add

---
+++ My project/Assets/Scripts/Level Editor/CrashDirector.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CrashDirector : MonoBehaviour
+{
+    [SerializeField] float delayTime = 0.5f;
+    [SerializeField] ParticleSystem crashEffect;
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Ground")
+        {
+            crashEffect.Play();
+            Invoke("ReloadScene", delayTime);
+        }
+    }
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+}   
